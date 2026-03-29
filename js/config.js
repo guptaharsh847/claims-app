223344f7909d876522e9767f9baf24d95ef4423b
Harsh Gupta <guptaharsh847@gmail.com>
2026-03-29 11:48:28 +0530

    updated with help indore

--- js/config.js
+++ js/config.js
@@ -1,7 +1,10 @@
 const CONFIG = {
-  API_URL:
-  " https://script.google.com/macros/s/AKfycbyVPFZVrOlOUilnOMHigBA93vlrMIZq-kuuKw4LE7RyVVr_bDX5ICRgYs3OQJ319RVR/exec",
-  // "https://script.google.com/macros/s/AKfycbwaXAA-wI-8h7iG29y5j31eYC1Xv_lDeCWl9FIkZDLP6ntCZfAgNhPDS_kkZXJIlVfQ/exec",
+  API_URL :
+  "https://script.google.com/macros/s/AKfycbzfpp0Eim_0fJEFXT5oTdyQilQfaNPH3QjxCQK852yePJKqII0fVsBvkE54dfLf2A0u/exec",
+
+  // API_URL:
+  // " https://script.google.com/macros/s/AKfycbyVPFZVrOlOUilnOMHigBA93vlrMIZq-kuuKw4LE7RyVVr_bDX5ICRgYs3OQJ319RVR/exec",
+  // // "https://script.google.com/macros/s/AKfycbwaXAA-wI-8h7iG29y5j31eYC1Xv_lDeCWl9FIkZDLP6ntCZfAgNhPDS_kkZXJIlVfQ/exec",
   ROLES: {
     ADMIN: "X9fK2pL5mQ8jR3t",
     USER: "B2vN9kM4lP7oJ5h",
--- js/script.js
+++ js/script.js
@@ -1,20 +1,22 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbwaXAA-wI-8h7iG29y5j31eYC1Xv_lDeCWl9FIkZDLP6ntCZfAgNhPDS_kkZXJIlVfQ/exec";
+// const API_URL = "https://script.google.com/macros/s/AKfycbwaXAA-wI-8h7iG29y5j31eYC1Xv_lDeCWl9FIkZDLP6ntCZfAgNhPDS_kkZXJIlVfQ/exec";
+const API_URL =
+  "https://script.google.com/macros/s/AKfycbzfpp0Eim_0fJEFXT5oTdyQilQfaNPH3QjxCQK852yePJKqII0fVsBvkE54dfLf2A0u/exec";
 
-document.getElementById("claimForm")?.addEventListener("submit", e => {
+document.getElementById("claimForm")?.addEventListener("submit", (e) => {
   e.preventDefault();
   const form = new FormData(e.target);
 
   fetch(API_URL, {
     method: "POST",
-    body: form
+    body: form,
   })
-  .then(res => res.text())
-  .then(msg => document.getElementById("result").innerText = msg);
+    .then((res) => res.text())
+    .then((msg) => (document.getElementById("result").innerText = msg));
 });
 
 function searchClaim() {
   const value = document.getElementById("searchValue").value;
   fetch(`${API_URL}?search=${value}`)
-    .then(res => res.text())
-    .then(data => document.getElementById("output").innerText = data);
+    .then((res) => res.text())
+    .then((data) => (document.getElementById("output").innerText = data));
 }
