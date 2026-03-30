dc0ec0b98512e00ba0bf4bd687bbac7d03765b71
Harsh Gupta <guptaharsh847@gmail.com>
2026-03-30 19:06:51 +0530

    fixed cache bug

--- sw.js
+++ sw.js
@@ -3,26 +3,26 @@ const ASSETS = [
   "./",
   "./index.html",
   "./login.html",
+  "./admin.html",
+  "./analytics.html",
+  "./manage-users.html",
+  "./add-user.html",
   "./claim.html",
   "./status.html",
   "./js/utils.js",
   "./js/api.js",
   "./js/config.js",
   "./js/auth.js",
+  "./js/admin.js",
   "./images/icon-192-192x192.png",
   "./images/icon-192-512x512.png",
 ];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(async (cache) => {
-      for (const asset of ASSETS) {
-        try {
-          await cache.add(asset);
-        } catch (err) {
-          console.error(`Failed to cache ${asset}:`, err);
-        }
-      }
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Service Worker: Caching app shell");
+      return cache.addAll(ASSETS);
     }),
   );
 });
